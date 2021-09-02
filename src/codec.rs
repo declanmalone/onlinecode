1ed544d7a8a7ff8ee5b96b1edf6e01457397dbaf
Declan Malone <idablack@users.sourceforge.net>
2021-09-02 20:48:50 +0100

    I was going to move some code in here, but maybe lib is better

---
+++ src/codec.rs
@@ -0,0 +1,8 @@
+// Things that are common to encoding, decoding
+//
+// * setting up parameters for transfer
+// * using rng to determine which blocks are in check block
+// * using rng to determine auxiliary mapping
+
+
+
